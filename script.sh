1a7fb8d38ab7c46b3410cc9cac86a62f1815333d
Dyeadal <dyeadal@gmail.com>
2023-11-30 19:29:39 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+while true; do
+  if [ -e "weekly_reboot.sh"]; then
+    read -p "Test reboot script before installing? (y/n): " testCheck
+  else:
+    echo "/sbin/reboot" > weekly_reboot.sh
+    chmod +x weekly_reboot.sh
+  fi
+
 
