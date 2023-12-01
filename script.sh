a213974d7651c614c6903eea28ab19dede47f6ed
Dyeadal <dyeadal@gmail.com>
2023-11-30 20:41:26 -0600

    Update script.sh
    Created functions to call on for tasks:
    - Verifying file existence and testing script
    - Verifying directory existence
    - Creating and adding cron job

--- script.sh
+++ script.sh
@@ -1,9 +1,52 @@
 #!/bin/bash
 
-while true; do
-  if [ -e "weekly_reboot.sh"]; then
-    read -p "Test reboot script before installing? (y/n): " testCheck
-  else:
+# variable for BASH script file name
+scriptFile="weekly_reboot.sh"
+
+# function to prompt admin if they want to test reboot script before moving file to cron directory
+testScript() {
+  read -p "Test reboot script before installing on system? (y/n)" testInput
+  case "$testInput" in
+    [yY])
+      echo "Rebooting"
+      ./weekly_reboot.sh
+    [nN])
+      echo "Not rebooting"
+      read -p "Test reboot script before installing on system? (y/n)" installInput
+      case "$installInput" in
+        [yY])
+          installScript
+        [nN])
+          echo "Exiting script"
+        esac
+  esac    
+}
+
+# function to install script
+installScript() {
+  createDir
+  # prompt for hour and day
+  echo " 0 - Sunday\n 1 - Monday\n 2 - Tuesday\n 3 - Wednesday\n 4 - Thursday\n 5 - Friday\n 6 - Saturday"
+  read -p "Reboot on which day?: " day
+  read -p "Reboot on which hour? (0-23): " hour
+
+  # add cron entry
+  cron_task = 0 $hour * * $day 0 $HOME/.scripts/$scriptfile
+  (crontab -l ; echo "$crontask") | crontab
+}
+
+createDir() {
+  if [ -d "~/.scripts" ]; then 
+  #creates .scripts directory on executing users home profile
+  mkdir $HOME/.scripts
+  cp $scriptfile $HOME/.scripts/
+}
+
+# checks if file exists, if not it creates it, also prompts if admin wants to test script
+while true; do 
+  if [ -e $scriptFile ]; then # file exists
+    testScript
+  else # file does NOT exist
     echo "/sbin/reboot" > weekly_reboot.sh
     chmod +x weekly_reboot.sh
   fi
